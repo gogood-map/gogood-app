141f175ae0bdfe9dc9bfb63ed496d3d5e9e60f93
Thiago <thiago.romaris@sptech.school>
2024-09-05 00:25:30 -0300

    tela de login pronta

--- app/src/main/java/com/example/gogood/login/Login.kt
+++ app/src/main/java/com/example/gogood/login/Login.kt
@@ -10,8 +10,10 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -20,15 +22,20 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -37,12 +44,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.gogood.ui.theme.GoGoodTheme
 import com.example.gogood.ui.theme.GogoodGray
+import com.example.gogood.ui.theme.GogoodGreen
 import com.example.gogood.ui.theme.GogoodWhite
 
 class Login : ComponentActivity() {
@@ -59,6 +71,7 @@ class Login : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginApp(modifier: Modifier = Modifier) {
     val emailState = remember { mutableStateOf("") }
@@ -93,14 +106,18 @@ fun LoginApp(modifier: Modifier = Modifier) {
             verticalArrangement = Arrangement.Center,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 16.dp, vertical = 100.dp)
+                .padding(horizontal = 16.dp, vertical = 70.dp)
         ) {
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(480.dp)
-                    .clip(RoundedCornerShape(16.dp))
-                    .background(Color.White)
+                    .shadow(
+                        elevation = 10.dp,
+                        shape = RoundedCornerShape(16.dp),
+                        spotColor = Color.Black.copy(alpha = 0.5f) // Decrease opacity here
+                    )
+                    .background(Color.White, RoundedCornerShape(16.dp))
             ) {
                 Column {
                     Row(
@@ -130,16 +147,20 @@ fun LoginApp(modifier: Modifier = Modifier) {
                             Column {
                                 Text(
                                     text = "Email*",
+                                    fontSize = 12.sp,
+                                    fontWeight = FontWeight.Bold,
                                     modifier = Modifier.padding(bottom = 4.dp)
                                 )
-
-                                OutlinedTextField(
+                                BasicTextField(
                                     value = emailState.value,
                                     onValueChange = { emailState.value = it },
                                     modifier = Modifier
-                                        .width(270.dp)
+                                        .width(250.dp)
                                         .height(45.dp)
                                         .border(1.dp, Color.Black, RoundedCornerShape(8.dp))
+                                        .background(Color.White, RoundedCornerShape(8.dp))
+                                        .padding(start = 15.dp, top = 15.dp),
+                                    singleLine = true,
                                 )
                             }
                         }
@@ -154,20 +175,26 @@ fun LoginApp(modifier: Modifier = Modifier) {
                             Column {
                                 Text(
                                     text = "Senha*",
+                                    fontSize = 12.sp,
+                                    fontWeight = FontWeight.Bold,
                                     modifier = Modifier.padding(bottom = 4.dp)
                                 )
-
-                                OutlinedTextField(
+                                BasicTextField(
                                     value = senhaState.value,
                                     onValueChange = { senhaState.value = it },
                                     modifier = Modifier
-                                        .width(270.dp)
+                                        .width(250.dp)
                                         .height(45.dp)
+                                        .border(1.dp, Color.Black, RoundedCornerShape(8.dp))
+                                        .background(Color.White, RoundedCornerShape(8.dp))
+                                        .padding(start = 15.dp, top = 15.dp),
+                                    singleLine = true,
                                 )
+
                             }
                         }
                     }
-                    Spacer(modifier = Modifier.height(16.dp))
+                    Spacer(modifier = Modifier.height(8.dp))
                     Row(
                         horizontalArrangement = Arrangement.Center,
                         verticalAlignment = Alignment.CenterVertically,
@@ -199,7 +226,7 @@ fun LoginApp(modifier: Modifier = Modifier) {
                         Column (horizontalAlignment = Alignment.CenterHorizontally) {
                             Text(
                                 text = "Ou faça parte com",
-                                fontSize = 14.sp,
+                                fontSize = 12.sp,
                                 fontWeight = FontWeight.Normal,
                             )
                             Spacer(modifier = Modifier.height(10.dp))
@@ -216,8 +243,13 @@ fun LoginApp(modifier: Modifier = Modifier) {
                             .fillMaxWidth()
                     ) {
                         Text(
-                            text = "É novo na nossa plataforma? Cadastrar",
-                            fontSize = 14.sp,
+                            buildAnnotatedString {
+                                append("É novo na nossa plataforma? ")
+                                withStyle(style = SpanStyle(color = GogoodGreen, textDecoration = TextDecoration.Underline)) {
+                                    append("Cadastrar")
+                                }
+                            },
+                            fontSize = 12.sp,
                             fontWeight = FontWeight.Normal,
                         )
                     }
