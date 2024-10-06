c7442bf0158a1a7523e00d4c6f23d4f222182e6f
TheusBr05 <mat.587263@gmail.com>
2024-10-06 02:44:01 -0300

    Refatora navegação do aplicativo

--- app/src/main/java/com/example/gogood/navegacao/AppNavegacao.kt
+++ app/src/main/java/com/example/gogood/navegacao/AppNavegacao.kt
@@ -1,9 +1,5 @@
 package com.example.gogood.navegacao
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -12,38 +8,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-
-import com.example.gogood.login.LoginApp
 import com.example.gogood.ui.telas.mapa.MapaDeCalor
 import com.example.gogood.menu.Menu
 import com.example.gogood.ui.telas.cadastro.CadastroTela
 import com.example.gogood.ui.telas.login.LoginTela
 import com.example.gogood.ui.theme.GoGoodTheme
-import kotlinx.serialization.Serializable
-
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            GoGoodTheme {
-                AppNavegacao()
-            }
-        }
-    }
-}
-
-@Serializable
-object MapaDeCalor
-
-@Serializable
-object Login
-
-@Serializable
-object Cadastro
-
-@Serializable
-object Menu
 
 @Composable
 fun AppNavegacao() {
@@ -52,19 +21,19 @@ fun AppNavegacao() {
     Scaffold { innerPadding ->
         NavHost(
             navController,
-            startDestination = MapaDeCalor,
+            startDestination = "MapaDeCalor",
             modifier = Modifier.padding(innerPadding)
         ) {
-            composable<MapaDeCalor> {
+            composable("MapaDeCalor") {
                 MapaDeCalor(navController)
             }
-            composable<Login> {
+            composable("Login") {
                 LoginTela(navController)
             }
-            composable<Cadastro> {
+            composable("Cadastro") {
                 CadastroTela(navController)
             }
-            composable<Menu> {
+            composable("Menu") {
                 Menu(navController)
             }
         }
