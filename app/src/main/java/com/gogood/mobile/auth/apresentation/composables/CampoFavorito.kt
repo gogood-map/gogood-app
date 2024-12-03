9cbed4429ddf2fca3a14d51b49b3fc2fe6ab6197
JohnVSCordeiro <joao.cordeiro@sptech.school>
2024-12-03 19:19:07 -0300

    @JohnVSCordeiro subindo alterações na parte visual da aplicação

--- app/src/main/java/com/gogood/mobile/auth/apresentation/composables/CampoFavorito.kt
+++ app/src/main/java/com/gogood/mobile/auth/apresentation/composables/CampoFavorito.kt
@@ -3,6 +3,7 @@ package com.gogood.mobile.auth.apresentation.composables
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -16,15 +17,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.gogood.mobile.ui.theme.GogoodOptionRed
-
 @Composable
-fun Campo(label: String, state: MutableState<String>, regraValida: (String)->Boolean,
-          validoParam: Boolean = true) {
+fun CampoFavorito(label: String, state: MutableState<String>, regraValida: (String) -> Boolean,
+                  validoParam: Boolean = true) {
     var valido by remember {
         mutableStateOf(validoParam)
     }
@@ -35,12 +36,11 @@ fun Campo(label: String, state: MutableState<String>, regraValida: (String)->Boo
         mutableStateOf("")
     }
 
-
     Column {
         Text(
             color = corInput,
             text = label,
-            fontSize = 12.sp,
+            fontSize = 16.sp,  // Aumentando o tamanho da fonte
             fontWeight = FontWeight.Bold
         )
 
@@ -48,36 +48,39 @@ fun Campo(label: String, state: MutableState<String>, regraValida: (String)->Boo
             value = state.value,
             onValueChange = {
                 state.value = it
-                if(label == "Email"){
+                if (label == "Email") {
                     it.trim()
                     state.value = state.value.trim()
                 }
                 valido = regraValida(state.value)
             },
             modifier = Modifier
-                .width(250.dp)
-                .height(45.dp)
-                .border(1.dp, corInput, RoundedCornerShape(8.dp))
-                .background(Color.White, RoundedCornerShape(8.dp))
+                .fillMaxWidth()
+                .width(342.dp)
+                .height(75.dp)
+                .padding(5.dp, 10.dp, 5.dp, 10.dp)
+                .border(0.5.dp, Color.Black, RoundedCornerShape(topStart = 5.dp))
+                .background(Color.White, RoundedCornerShape(5.dp, 0.dp, 0.dp, 0.dp))
+                .alpha(1f)
                 .padding(start = 15.dp, top = 15.dp),
             singleLine = true,
         )
-        if(!valido){
+        if (!valido) {
             corInput = GogoodOptionRed
-            if(state.value.isBlank()){
+            if (state.value.isBlank()) {
                 textoErro = "Campo obrigatório"
-            }else{
-                when(label){
-                    "Email"->{
+            } else {
+                when (label) {
+                    "Email" -> {
                         textoErro = "Email inválido"
                     }
-                    "Nome"->{
+                    "Nome" -> {
                         textoErro = "Utilize 6 ou mais caracteres"
                     }
                 }
             }
             Text(text = textoErro, color = corInput, fontSize = 10.sp)
-        }else{
+        } else {
             corInput = Color.Black
         }
     }
